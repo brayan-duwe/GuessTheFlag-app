2957fa312bd2288adf337e86537fbbe4e73db633
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-03-07 18:59:00 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Brayan Duwe on 07/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
